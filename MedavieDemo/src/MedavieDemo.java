45e8577b4a27cf256d1241d02ed5631a584e4093
zaheersaib <101590223+zaheersaib@users.noreply.github.com>
2022-07-09 15:56:44 -0400

    Update MedavieDemo.java

--- MedavieDemo/src/MedavieDemo.java
+++ MedavieDemo/src/MedavieDemo.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
-import Currency.Currency;
 import Managers.FanManager;
 import Models.Fan;
 
